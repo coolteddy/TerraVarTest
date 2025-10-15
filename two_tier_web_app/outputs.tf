7b0a256f0861d23ed73d569219bbb7de1c01bd89
Thet Naing <coolteddy@gmail.com>
2025-10-15 18:53:30 +0100

    added rds and subnet groups

--- two_tier_web_app/outputs.tf
+++ two_tier_web_app/outputs.tf
@@ -1,7 +1,13 @@
 ############################
 # Outputs
 ############################
-# output "alb_dns_name" {
-#   value       = aws_lb.alb.dns_name
-#   description = "Public URL of the app (give it ~60s after apply)."
-# }
+output "alb_dns_name" {
+  value       = aws_lb.alb.dns_name
+  description = "Public URL for the application."
+}
+
+output "rds_endpoint" {
+  value       = aws_db_instance.main.endpoint
+  description = "RDS instance endpoint"
+  sensitive   = true
+}
