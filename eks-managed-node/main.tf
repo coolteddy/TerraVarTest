4fa2e3247733cbde5586ae4353455a9fc371bd7f
Thet Naing <coolteddy@gmail.com>
2025-10-21 18:25:28 +0100

    added IRSA for VPC CNI and added sample S3 role IRSA

--- eks-managed-node/main.tf
+++ eks-managed-node/main.tf
@@ -1,3 +1,5 @@
+
+
 locals {
   tags = {
     Project = "eks-demo"
@@ -180,6 +182,7 @@ resource "aws_route_table_association" "private_b" {
 ############################
 # IAM: EKS Cluster & NodeGroup roles
 ############################
+
 # Cluster role
 resource "aws_iam_role" "cluster" {
   name = "${var.cluster_name}-cluster-role"
@@ -233,6 +236,31 @@ resource "aws_iam_role_policy_attachment" "node_AmazonEKS_CNI_Policy" {
 ############################
 # EKS Cluster
 ############################
+
+# custom security group for the cluster control plane
+# resource "aws_security_group" "eks_cluster" {
+#   name        = "${var.cluster_name}-cluster-sg"
+#   description = "EKS Cluster API server security group"
+#   vpc_id      = aws_vpc.this.id
+
+#   ingress {
+#     from_port   = 443
+#     to_port     = 443
+#     protocol    = "tcp"
+#     cidr_blocks = ["your-corporate-cidr", aws_vpc.this.cidr_block]
+#   }
+
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   tags = local.tags
+# }
+
+
 resource "aws_eks_cluster" "this" {
   name     = var.cluster_name
   role_arn = aws_iam_role.cluster.arn
@@ -246,6 +274,7 @@ resource "aws_eks_cluster" "this" {
       aws_subnet.private_b.id
     ]
     # You can set cluster_security_group_id here; if omitted, AWS creates one.
+    # cluster_security_group_id = aws_security_group.eks_cluster.id
   }
 
   tags = local.tags
@@ -401,3 +430,165 @@ resource "helm_release" "alb_controller" {
     aws_eks_cluster.this
   ]
 }
+
+
+############################
+# IRSA for VPC CNI Plugin (aws-node)
+############################
+
+# 1. IAM Policy for VPC CNI
+resource "aws_iam_policy" "vpc_cni" {
+  name   = "AmazonEKS_CNI_Policy"
+  description = "IAM policy for EKS VPC CNI plugin"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:DescribeSubnets",
+        "ec2:DescribeSecurityGroups",
+        "ec2:DescribeDhcpOptions",
+        "ec2:DescribeVpcs",
+        "ec2:DescribeInstances",
+        "ec2:DescribeNetworkInterfaces",
+        "ec2:CreateNetworkInterface",
+        "ec2:DeleteNetworkInterface",
+        "ec2:AttachNetworkInterface",
+        "ec2:DetachNetworkInterface",
+        "ec2:ModifyNetworkInterfaceAttribute",
+        "ec2:AssignPrivateIpAddresses",
+        "ec2:UnassignPrivateIpAddresses"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+# 2. Trust Policy for VPC CNI IRSA
+data "aws_iam_policy_document" "vpc_cni_trust" {
+  statement {
+    effect = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.eks.arn]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(local.oidc_issuer, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:aws-node"]
+    }
+  }
+}
+
+# 3. IAM Role for VPC CNI
+
+resource "aws_iam_role" "vpc_cni" {
+  name               = "eks-vpc-cni-irsa"
+  assume_role_policy = data.aws_iam_policy_document.vpc_cni_trust.json
+  tags = local.tags
+}
+
+# 4. Attach Policy to Role
+resource "aws_iam_role_policy_attachment" "vpc_cni_attach" {
+  role       = aws_iam_role.vpc_cni.name
+  policy_arn = aws_iam_policy.vpc_cni.arn
+}
+
+# 5. Annotate aws-node ServiceAccount (Kubernetes)
+# looks like already created by EKS or Helm chart
+# resource "kubernetes_service_account" "aws_node" {
+#   metadata {
+#     name      = "aws-node"
+#     namespace = "kube-system"
+#     annotations = {
+#       "eks.amazonaws.com/role-arn" = aws_iam_role.vpc_cni.arn
+#     }
+#     labels = {
+#       "app.kubernetes.io/name" = "aws-node"
+#     }
+#   }
+#   automount_service_account_token = true
+# }
+
+# manually run this after commented out aws-node service account
+# kubectl annotate serviceaccount aws-node \
+#   -n kube-system \
+#   eks.amazonaws.com/role-arn=<paste-the-arn-here> --overwrite
+
+
+############################
+# IRSA Example: Pod Access to S3 access
+############################
+
+# # 1. IAM Policy for S3 access (replace bucket name as needed)
+# resource "aws_iam_policy" "s3_access" {
+#   name        = "eks-s3-access-policy"
+#   description = "Allow pod to access specific S3 bucket"
+#   policy      = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Effect": "Allow",
+#       "Action": ["s3:GetObject", "s3:ListBucket"],
+#       "Resource": [
+#         "arn:aws:s3:::your-bucket-name",
+#         "arn:aws:s3:::your-bucket-name/*"
+#       ]
+#     }
+#   ]
+# }
+# EOF
+# }
+
+# # 2. Trust Policy for IRSA (service account in app namespace)
+# data "aws_iam_policy_document" "s3_access_trust" {
+#   statement {
+#     effect = "Allow"
+#     actions = ["sts:AssumeRoleWithWebIdentity"]
+#     principals {
+#       type        = "Federated"
+#       identifiers = [aws_iam_openid_connect_provider.eks.arn]
+#     }
+#     condition {
+#       test     = "StringEquals"
+#       variable = "${replace(local.oidc_issuer, "https://", "")}:sub"
+#       values   = ["system:serviceaccount:app:s3-access-sa"]
+#     }
+#   }
+# }
+
+# # 3. IAM Role for S3 access
+# resource "aws_iam_role" "s3_access" {
+#   name               = "eks-s3-access-role"
+#   assume_role_policy = data.aws_iam_policy_document.s3_access_trust.json
+#   tags = local.tags
+# }
+
+# # 4. Attach Policy to Role
+# resource "aws_iam_role_policy_attachment" "s3_access_attach" {
+#   role       = aws_iam_role.s3_access.name
+#   policy_arn = aws_iam_policy.s3_access.arn
+# }
+
+# # 5. Annotate ServiceAccount for pod (namespace: app)
+# resource "kubernetes_service_account" "s3_access_sa" {
+#   metadata {
+#     name      = "s3-access-sa"
+#     namespace = "app"
+#     annotations = {
+#       "eks.amazonaws.com/role-arn" = aws_iam_role.s3_access.arn
+#     }
+#     labels = {
+#       "app.kubernetes.io/name" = "s3-access"
+#     }
+#   }
+#   automount_service_account_token = true
+# }
+
+# To use: Set your pod/deployment spec to use serviceAccountName: s3-access-sa in the app namespace.
